f60383dd2d29a2755331922b641c4fcda1730218
recetasdevops <44634828+recetasdevops@users.noreply.github.com>
2019-07-21 18:09:28 +0200

    Create main.tf

---
+++ src/AzureWebApp/main.tf
@@ -0,0 +1,30 @@
+#Definimos el resource group a crear
+resource "azurerm_resource_group" "resource_group_test" {
+  name     = "TFRecetasDevOpsAppServiceRG"
+  location = "West Europe"
+}
+
+#Definimos el service plan a crear
+resource "azurerm_app_service_plan" "app_service_test" {
+  name                = "recetasdevopsserviceplan"
+  location            = azurerm_resource_group.resource_group_test.location
+  resource_group_name = azurerm_resource_group.resource_group_test.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+#Definimos el app service a crear
+resource "azurerm_app_service" "test" {
+  name                = "recetasdevopsterraform"
+  location            = azurerm_resource_group.resource_group_test.location
+  resource_group_name = azurerm_resource_group.resource_group_test.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_test.id
+
+  app_settings = {
+    "ApiUrl" = "www.google.es",
+    "Version" = "1.0"
+  }
+}
